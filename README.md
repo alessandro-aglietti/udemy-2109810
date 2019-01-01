3e0a943d4760f7331d7b0be0d174de45bef64fd3
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2019-01-01 21:19:49 +0100

    dumping merkle block

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # udemy-2109810
+
+node spv.js
+
+tail -f connect-test/SPV/testnet/debug.log
+
+per evitare che si connetta a nodi diversi dal nostro
+rm connect-test/SPV/testnet/hosts.json
--- spv.js
+++ spv.js
@@ -3,10 +3,13 @@
 // necessary for portability
 const os = require('os');
 const path = require('path');
+const fs = require('fs');
 
 const bcoin = require('bcoin').set('testnet');
+// expose https://bcoin.io/docs/bcoin.js.html#line51
 const NetAddress = bcoin.net.NetAddress;
 const Network = bcoin.Network;
+const MerkleBlock = bcoin.MerkleBlock;
 const pEvent = require('p-event'); // tool to await for events
 
 async function delay(ms) {
@@ -15,13 +18,18 @@ async function delay(ms) {
   });
 }
 
-const testnet = Network.get().toString();
-const logPrefix = path.join(os.homedir(), 'connect-test');
+const testnet = Network.get();
+// bcoin didn't allow to disable seeds
+// https://bcoin.io/docs/net_pool.js.html#line3633
+testnet.seeds = [];
+
+const logPrefix = path.join(__dirname, 'connect-test');
 
 // create nodes
 const spvNode = new bcoin.SPVNode({
-  network: testnet,
-  httpPort: 48449, // avoid clash of ports
+  network: testnet.toString(),
+  // bcoin can't disable http/rpc liste
+  httpPort: 18331,
 
   // write log file and chain data to specific directory
   prefix: path.join(logPrefix, 'SPV'),
@@ -32,6 +40,8 @@ const spvNode = new bcoin.SPVNode({
 
   // reduce log spam on SPV node (cannot reduce to 0 for full node)
   maxOutbound: 1,
+  nodes: [],
+  seeds: [],
 });
 
 // nodes created!
@@ -51,13 +61,30 @@ const spvNode = new bcoin.SPVNode({
   spvNode.startSync();
 
   // SPV node: watch this address
-  const address = bcoin.Address.fromString('R9M3aUWCcKoiqDPusJvqNkAbjffLgCqYip', spvNode.network);
+  // enabling the bloom filter
+  // https://bcoin.io/docs/net_pool.js.html#line89
+  // se non viene passata l'opzione copia quello della chain
+  // https://bcoin.io/docs/net_pool.js.html#line3693
+  const address = bcoin.Address.fromString('n4qKPDRyiWjCriqEGyffzraXq6bqyf2fws', spvNode.network);
   spvNode.pool.watchAddress(address);
 
   // SPV node: catch tx events
   spvNode.on('tx', (tx) => {
-    console.log('-- SPV node received tx: --\n', tx);
+    const txHash = tx.hash('hex');
+    fs.writeFileSync(`${__dirname}/tx_${txHash}.json`, JSON.stringify(tx.toJSON(), null, 2));
+    console.log(`-- SPV node received tx: -- ${txHash}`);
   });
+
+  spvNode.on('block', (block) => {
+      if (MerkleBlock.isMerkleBlock(block)) {
+        const blockHash = block.hash('hex');
+        fs.writeFileSync(`${__dirname}/block_${blockHash}.json`, JSON.stringify(block.toJSON(), null, 2));
+        console.log(`-- SPV node received MERKLEBLOCK: -- ${blockHash}`);
+      } else {
+        console.log(`-- SPV node received block: -- ${block.hash}`);
+      }
+  });
+
 
   // allow some time for spvNode to figure
   // out that its peer list is empty
@@ -81,6 +108,8 @@ const spvNode = new bcoin.SPVNode({
   // nodes are now connected!
 
   console.log('spvNode\'s peers after connection:', spvNode.pool.peers.head());
+
+  await pEvent(spvNode.pool, 'peer close');
 
   // closing nodes
   await spvNode.disconnect();
